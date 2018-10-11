072745368ffdf59d0c5321b77996bcfda7f992b6
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2018-10-11 12:25:39 -0800

    zombie will walk when space is held

---
+++ ZombieRun/Assets/Scripts/playerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    public Animator ainm;
+
+	void Start () {
+        ainm = GetComponent<Animator>();
+	}
+	
+
+	void Update () {
+        if (Input.GetKey("space"))
+        {
+            ainm.SetBool("isWalking", true);
+            Debug.Log("walking");
+        }
+        else
+        {
+            ainm.SetBool("isWalking", false);
+            Debug.Log("idle");
+        }
+	}
+}
