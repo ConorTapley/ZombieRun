361ca777e1e984b9f017627d3a7587eb3288f3c6
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2018-10-30 08:58:22 -0800

    jump
    Jumping Works

--- ZombieRun/Assets/Scripts/PlayerMotor.cs
+++ ZombieRun/Assets/Scripts/PlayerMotor.cs
@@ -4,35 +4,43 @@ using UnityEngine;
 
 public class PlayerMotor : MonoBehaviour {
 
+    private Animator ainm;
     private CharacterController m_controller;
     private Vector3 m_moveVector;
+    private Transform m_transform;
 
     public float speed;
-    private float m_verticalVelocity = 0.0f;
-    private float m_gravity = 500f;
-
-
-
-
+    public float jumpPower;
+    private float m_verticalVelocity;
+    public float gravity;
+    
 
     void Start () {
         m_controller = GetComponent<CharacterController>();
+        ainm = GetComponent<Animator>();
 	}
 	
-
 
 	
 	void Update () {
         m_moveVector = Vector3.zero;
-
+        
 
         if(m_controller.isGrounded)
         {
             m_verticalVelocity = -0.5f;
+
+            if(Input.GetKey(KeyCode.W))
+            {
+                ainm.SetBool("Jump", true);
+                m_verticalVelocity += jumpPower;
+                Debug.Log("Jump");
+            }
         }
         else
         {
-            m_verticalVelocity -= m_gravity * Time.deltaTime;
+            m_verticalVelocity -= gravity * Time.deltaTime;
+            ainm.SetBool("Jump", false);
         }
 
 
