f8560de39a16491afa5d7a0f1af75b1de22e5348
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2018-10-30 07:23:40 -0800

    gravity
    Player is affected by gravity

--- ZombieRun/Assets/Scripts/PlayerMotor.cs
+++ ZombieRun/Assets/Scripts/PlayerMotor.cs
@@ -4,27 +4,48 @@ using UnityEngine;
 
 public class PlayerMotor : MonoBehaviour {
 
-    private CharacterController controller;
-    private Vector3 moveVector;
+    private CharacterController m_controller;
+    private Vector3 m_moveVector;
 
     public float speed;
+    private float m_verticalVelocity = 0.0f;
+    private float m_gravity = 500f;
 
-	// Use this for initialization
-	void Start () {
-        controller = GetComponent<CharacterController>();
+
+
+
+
+    void Start () {
+        m_controller = GetComponent<CharacterController>();
 	}
 	
-	// Update is called once per frame
+
+
+	
 	void Update () {
-        moveVector = Vector3.zero;
+        m_moveVector = Vector3.zero;
+
+
+        if(m_controller.isGrounded)
+        {
+            m_verticalVelocity = -0.5f;
+        }
+        else
+        {
+            m_verticalVelocity -= m_gravity * Time.deltaTime;
+        }
+
 
         //X Left Right
-        moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
+        m_moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
+
         //Y Up Down
+        m_moveVector.y = m_verticalVelocity;
 
         //Z Forward Backward  
-        moveVector.z = speed;
+        m_moveVector.z = speed;
 
-        controller.Move(moveVector * Time.deltaTime);
+
+        m_controller.Move(m_moveVector * Time.deltaTime);
 	}
 }
