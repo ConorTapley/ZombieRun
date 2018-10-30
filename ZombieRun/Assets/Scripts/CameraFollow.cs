8388c6e388dd39faeaab7516f721e76f825c1ef6
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2018-10-30 08:02:09 -0800

    Camera
    Camera follows player doesn't move left and right

---
+++ ZombieRun/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    private Transform m_lookAt;
+    private Vector3 m_startOffset;
+    private Vector3 m_moveVector;
+
+    void Start()
+    {
+        m_lookAt = GameObject.FindGameObjectWithTag("Player").transform;
+        m_startOffset = transform.position - m_lookAt.position;
+    }
+
+
+    void Update()
+    {
+        m_moveVector = m_lookAt.position + m_startOffset;
+
+        //X
+        m_moveVector.x = 0;
+
+        //Y
+        m_moveVector.y = Mathf.Clamp(m_moveVector.y, 60, 70);
+
+        transform.position = m_moveVector;
+    }
+}
