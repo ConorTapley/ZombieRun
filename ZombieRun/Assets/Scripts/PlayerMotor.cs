e944dbc37b6fc4e0e127682586073f3d1e54ab99
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2018-10-30 06:53:02 -0800

    Movement
    Player moves forward automatically can move left and right

---
+++ ZombieRun/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour {
+
+    private CharacterController controller;
+    private Vector3 moveVector;
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveVector = Vector3.zero;
+
+        //X Left Right
+        moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
+        //Y Up Down
+
+        //Z Forward Backward  
+        moveVector.z = speed;
+
+        controller.Move(moveVector * Time.deltaTime);
+	}
+}
